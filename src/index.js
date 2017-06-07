7e7c06668140ee2779d47c2d64c6740572c56806
Sofia Nguy <sofianguy@gmail.com>
2017-06-07 15:03:16 -0700

    hello world

--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
