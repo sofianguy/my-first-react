50ecfe6fe9537ff1ffa5b02d27ccadef78302501
Sofia Nguy <sofianguy@gmail.com>
2017-06-08 19:32:30 -0700

    try using JSX expressions

--- src/index.js
+++ src/index.js
@@ -7,7 +7,23 @@ import './index.css';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'sofia',
+  lastName: 'nguy'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
 );
